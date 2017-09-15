26d7d4214232a7f9b0a23dba7b89341d95366726
Randall Lunetto <rlunetto@gmail.com>
2017-09-14 22:23:22 -0700

    Update gravatar.js

--- src/hooks/gravatar.js
+++ src/hooks/gravatar.js
@@ -1,26 +1,27 @@
 'use strict';
 
-// src\services\user\hooks\gravatar.js
-//
 // Use this hook to manipulate incoming or outgoing data.
-// For more information on hooks see: http://docs.feathersjs.com/hooks/readme.html
+// For more information on hooks see: http://docs.feathersjs.com/api/hooks.html
 
-const defaults = {};
+// We need this to create the MD5 hash
 const crypto = require('crypto');
 
+// The Gravatar image service
 const gravatarUrl = 'https://s.gravatar.com/avatar';
-const query = `s=60`;
+// The size query. Our chat needs 60px images
+const query = 's=60';
 
-const gravatarImage = email => {
-  const hash = crypto.createHash('md5').update(email).digest('hex');
-
-  return `${gravatarUrl}/${hash}?${query}`;
-}
-
-module.exports = function(options) {
+module.exports = function() {
   return function(hook) {
-    hook.data = Object.assign({}, hook.data, {
-      avatar: gravatarImage(hook.data.email)
-    });
+    // The user email
+    const { email } = hook.data;
+    // Gravatar uses MD5 hashes from an email address to get the image
+    const hash = crypto.createHash('md5').update(email).digest('hex');
+
+    hook.data.avatar = `${gravatarUrl}/${hash}?${query}`;
+
+    // Hooks can either return nothing or a promise
+    // that resolves with the `hook` object for asynchronous operations
+    return Promise.resolve(hook);
   };
 };
